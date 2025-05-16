baa5423363e97613e02308ed149c6309fa84535f
christianokuru <okuruchristian@gmail.com>
2025-05-16 11:56:03 +0100

    installed shadcn-vue properly

--- components/custom/website/NavbarComponent.vue
+++ components/custom/website/NavbarComponent.vue
@@ -1,7 +1,8 @@
 <script setup>
-
+import { Button } from '@/components/ui/button'
 </script>
 
 <template>
     <h1 class="text-3xl font-bold text-red-500 underline">The Navbar componentss</h1>
+    <Button class="ml-10 rounded-none">Button</Button>
 </template>
