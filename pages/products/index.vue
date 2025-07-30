3e18dc515844d04505693d6ac60ec7401e13489c
isaac-hash <zikkychukwudulue@gmail.com>
2025-07-30 16:19:50 +0100

    added pagination to products

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,16 +1,34 @@
+
 <script setup>
+import { ref, computed } from "vue";
 import { products } from "~/lib/products";
 import ProductCardComponent from "@/components/custom/website/ProductCardComponent.vue";
 import { useCartStore } from "@/stores/cart";
+import { Pagination, PaginationContent, PaginationItem, PaginationPrevious, PaginationNext } from "@/components/ui/pagination";
 
 useHead({
   title: "All Our Products",
 });
 
 const cartStore = useCartStore();
-
 const addToCart = (product) => {
   cartStore.addToCart(product);
+};
+
+// Pagination logic
+const currentPage = ref(1);
+const pageSize = 8;
+const totalPages = computed(() => Math.ceil(products.length / pageSize));
+const paginatedProducts = computed(() => {
+  const start = (currentPage.value - 1) * pageSize;
+  return products.slice(start, start + pageSize);
+});
+
+const goToPrev = () => {
+  if (currentPage.value > 1) currentPage.value--;
+};
+const goToNext = () => {
+  if (currentPage.value < totalPages.value) currentPage.value++;
 };
 </script>
 
@@ -20,11 +38,28 @@ const addToCart = (product) => {
 
     <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
       <ProductCardComponent
-        v-for="product in products"
+        v-for="product in paginatedProducts"
         :key="product.id"
         :product="product"
         :on-add-to-cart="() => addToCart(product)"
       />
+    </div>
+
+    <div class="flex justify-center mt-10">
+      <Pagination :total="totalPages" :page="currentPage" class="">
+        <PaginationContent>
+          <PaginationPrevious @click="goToPrev" :disabled="currentPage === 1" />
+          <PaginationItem
+            v-for="page in totalPages"
+            :key="page"
+            :is-active="page === currentPage"
+            @click="currentPage = page"
+          >
+            {{ page }}
+          </PaginationItem>
+          <PaginationNext @click="goToNext" :disabled="currentPage === totalPages" />
+        </PaginationContent>
+      </Pagination>
     </div>
   </div>
 </template>
